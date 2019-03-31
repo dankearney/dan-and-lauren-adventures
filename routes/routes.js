17571543bcebceef68401ca8e5634bfd0df06db4
dankearney <danielpatrickkearney@gmail.com>
2019-03-31 17:12:59 -0400

    NH48 tab added

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/NH48', function(req, res) {
+  res.render('NH48');
 });
 
 module.exports = router;
